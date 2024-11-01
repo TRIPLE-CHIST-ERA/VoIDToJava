2636e4a2293e1d47f0522e57d7a75bf12b2b3375
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-11-01 10:15:10 +0100

    Leave spring-boot bean generation for later

--- src/main/java/swiss/sib/swissprot/chistera/triples/GenerateJavaFromVoID.java
+++ src/main/java/swiss/sib/swissprot/chistera/triples/GenerateJavaFromVoID.java
@@ -108,12 +108,6 @@ public class GenerateJavaFromVoID {
 			<?xml version="1.0" encoding="UTF-8"?>
 			<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
 			  <modelVersion>4.0.0</modelVersion>
-			  <parent>
-				<groupId>org.springframework.boot</groupId>
-				<artifactId>spring-boot-starter-parent</artifactId>
-				<version>3.3.5</version>
-				<relativePath/>
-			  </parent>
 			  <groupId>${groupId}</groupId>
 			  <artifactId>${artifactId}</artifactId>
 			  <version>${version}</version>
